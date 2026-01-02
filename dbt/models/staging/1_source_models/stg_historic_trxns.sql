10106a1f3514c1a4a8c70c8067b39640708ef364
MarcelloMolinaro <marcelloridesabike@gmail.com>
2026-01-02 01:41:39 -0500

    updates the unique_key to be hexadecimal - much better

--- dbt/models/staging/1_source_models/stg_historic_trxns.sql
+++ dbt/models/staging/1_source_models/stg_historic_trxns.sql
@@ -23,7 +23,11 @@ source as ( select * from {{ ref('historic_transactions') }} )
             source.account_name, -- remove this if you want to force a mapping
             'Missing mapping! Add to seed_account_mapping_historic.csv'
         ) as owner_name,
-        row_number() over (order by source.transaction_date) as original_row_number
+        coalesce(source.account_name, '') ||
+            coalesce(source.amount::text, '') ||
+            coalesce(source.transaction_date::text, '') ||
+            coalesce(source.description, '')
+        as base_transaction_id
     from source
     left join account_mapping
         on source.account_name = account_mapping.account_name
@@ -34,36 +38,45 @@ source as ( select * from {{ ref('historic_transactions') }} )
         )
 )
 
+, dupe_row_nums as (
+
+    select 
+        *
+        , row_number() over (
+            partition by base_transaction_id 
+            order by transaction_date
+        ) as duplicates_row_number
+    from accounts_mapped
+
+)
+
+
 , final as (
 
     select
-        -- Create transaction_id from account_name | amount | transaction_date | description
-        ('HIST_TRN_' ||
-        MD5(coalesce(account_name, '') ||
-            coalesce(amount::text, '') ||
-            coalesce(transaction_date::text, '') ||
-            coalesce(description, '') ||
-            coalesce(original_row_number::text, ''))
-        )::text                                as transaction_id,  
-        null::text                             as account_id,
-        account_name::text                     as original_account_name,
-        mapped_account_name::text              as account_name,
-        additional_account_detail::text        as detailed_account_name,
-        owner_name::text                       as owner_name,
-        null::text                             as institution_domain,
-        null::text                             as institution_name,
-        amount::numeric                        as amount,
-        null::timestamp                        as posted,
-        null::date                             as posted_date,
-        null::timestamp                        as transacted_at,
-        transaction_date::date                 as transacted_date,
-        description::text                      as description,
-        null::boolean                          as pending,
-        source_category::text                  as source_category,
-        master_category::text                  as master_category,
-        null::timestamp                        as import_timestamp,
-        to_date(input_date, 'MM/DD/YYYY')      as import_date
-    from accounts_mapped
+        -- Create transaction_id from account_name | amount | transaction_date | description 
+        -- ... and duplicates_row_number when needed
+        'HIST_TRN_' || MD5(base_transaction_id || 
+            duplicates_row_number::text)      as transaction_id,
+        null::text                          as account_id,
+        account_name::text                  as original_account_name,
+        mapped_account_name::text           as account_name,
+        additional_account_detail::text     as detailed_account_name,
+        owner_name::text                    as owner_name,
+        null::text                          as institution_domain,
+        null::text                          as institution_name,
+        amount::numeric                     as amount,
+        null::timestamp                     as posted,
+        null::date                          as posted_date,
+        null::timestamp                     as transacted_at,
+        transaction_date::date              as transacted_date,
+        description::text                   as description,
+        null::boolean                       as pending,
+        source_category::text               as source_category,
+        master_category::text               as master_category,
+        null::timestamp                     as import_timestamp,
+        to_date(input_date, 'MM/DD/YYYY')   as import_date
+    from dupe_row_nums
 
 )
 
